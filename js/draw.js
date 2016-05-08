0736e861dff9e5dba433f4fa4fb125c0534cf1fa
mmelki <mmelki@kapit.fr>
2016-05-08 19:08:57 +0100

    Add board Grid style

---
+++ js/draw.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function () {
+
+    var board = $('#board');
+
+    var board_width = board.width();
+    var board_height = board.height();
+
+    var cell = "<td class='cell'></td>";
+
+    for (let i = 0; i <= board_height; i += 60) {
+        for (let j = 60; j <= board_width; j += 60) {
+            board.append(cell);
+        }
+    }
+});
