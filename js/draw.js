0726b8ee9693dcd6790e09c7326aa3b6ab826264
mmelki <mmelki@kapit.fr>
2016-08-20 14:50:05 +0100

    Fix grid generation

--- js/draw.js
+++ js/draw.js
@@ -1,19 +1,22 @@
-
 $(document).ready(function () {
 
-    drawGrill();
+    drawGrid();
 });
-function drawGrill() {
-    var board = $('#board');
+function drawGrid() {
 
-    var board_width = board.width();
-    var board_height = board.height();
+    var grid = $('#grid');
+    var board = $('div.keyboard');
+
+    const TOP_NAV_HEIGHT = 78;
+
+    var grid_width = grid.width();
+    var grid_height = board.height() + TOP_NAV_HEIGHT;
 
     var cell = "<td class='cell'></td>";
 
-    for (let i = 0; i <= board_height; i += 60) {
-        for (let j = 60; j <= board_width; j += 60) {
-            board.append(cell);
+    for (let i = 0; i <= grid_height; i += 60) {
+        for (let j = 60; j <= grid_width; j += 60) {
+            grid.append(cell);
         }
     }
 }
