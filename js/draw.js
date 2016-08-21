41654756d37239d8e5bd0d3ab1d531b6da327b4b
mmelki <mmelki@kapit.fr>
2016-08-21 15:53:19 +0100

    Move to next cell when putting a key in grid

--- js/draw.js
+++ js/draw.js
@@ -4,9 +4,9 @@ var selectedCell;
 $(document).ready(function () {
 
     drawGrid();
-    flagCurrentCell();
 
-    copyKeyToSelectedCell();
+    addCellClickBehavior(); // flag current cell selected
+    addKeysClickBehavior();
 
 });
 
@@ -35,24 +35,51 @@ function drawGrid() {
     });
 }
 
-function flagCurrentCell() {
 
+function addCellClickBehavior() {
     $("#grid td.cell").each(function () {
         $(this).click(function () {
-            $(this).addClass("cell-selected");
-            if (selectedCell && this !== selectedCell) {
-                $(selectedCell).removeClass("cell-selected");
-            }
-            selectedCell = this;
+            flagCell($(this));
         });
+    });
+}
+
+function flagCell(cell) {
+
+    cell.addClass("cell-selected");
+    if (selectedCell && cell !== selectedCell) {
+        selectedCell.removeClass("cell-selected");
+    }
+    selectedCell = cell;
+}
+
+function deleteSelectedCell() {
+    if (selectedCell) {
+        selectedCell.html("");
+        if (selectedCell.prev("a.key-cloned")) {
+            flagCell(selectedCell.prev("a.key-cloned"));
+        }
+    }
+}
+function addKeysClickBehavior() {
+
+    // all keys
+    $('ul.qwerty li a').click(function () {
+        copyKeyToSelectedCell(this);
+    });
+
+    // delete key
+    $('#delete-key').click(function () {
+        deleteSelectedCell();
     });
 
 }
 
-function copyKeyToSelectedCell() {
-    $('ul.qwerty li a').click(function () {
-        if (selectedCell) {
-            $(selectedCell).html($(this).clone().addClass("key-cloned"));
+function copyKeyToSelectedCell(element) {
+    if (selectedCell) {
+        selectedCell.html($(element).clone().addClass("key-cloned"));
+        if (selectedCell.next("td.cell")) {
+            flagCell(selectedCell.next("td.cell"));
         }
-    });
+    }
 }
