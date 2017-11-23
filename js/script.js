d62cb36f8c461e6eaaff1d624338ccb6f470b925
mmelki <mmelki@kapit.fr>
2017-11-23 10:40:46 +0100

    Reindent code with 2spaces base

--- js/script.js
+++ js/script.js
@@ -1,72 +1,71 @@
 $(document).ready(function () {
 
-    $('#keyboard_icon').click(function () {
-        toggleAlphabeticKeyboard();
-    });
+  $('#keyboard_icon').click(function () {
+    toggleAlphabeticKeyboard();
+  });
 
-    addScrollButtonsBehavior();
-    addSelectionButtonBehavior();
+  addScrollButtonsBehavior();
+  addSelectionButtonBehavior();
 
-    //drawGraph();
+  //drawGraph();
 });
 
 
 function toggleAlphabeticKeyboard() {
 
-    var keys = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
-        'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  var keys = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+    'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
-    var content = "<li><a class='key'><span>$key</span></a></li>";
+  var content = "<li><a class='key'><span>$key</span></a></li>";
 
-    var special_keys_content = "<li><a class='key space-key'><span class='key-span' style='background: url(\"img\/keys\/space%20bar.png\");'></span></a></li><li><a class='key return-key'><span class='key-span' style='background: url(\"img\/keys\/return.png\");'></span></a></li> <li><a class='key delete-key'><span class='key-span' style='background: url(\"img\/keys\/delete%20backspace.png\");'></span></a></li>";
+  var special_keys_content = "<li><a class='key space-key'><span class='key-span' style='background: url(\"img\/keys\/space%20bar.png\");'></span></a></li><li><a class='key return-key'><span class='key-span' style='background: url(\"img\/keys\/return.png\");'></span></a></li> <li><a class='key delete-key'><span class='key-span' style='background: url(\"img\/keys\/delete%20backspace.png\");'></span></a></li>";
 
-    $("ul.qwerty li").toggle();
+  $("ul.qwerty li").toggle();
 
-    if (!$("ul.qwerty li").is(":visible")) {
-        // show alphabet keyboard
-        for (var i = 0; i < keys.length; i++) {
-            if (i < 14) {
-                $("div.keyboard-left ul.qwerty").append(content.replace("$key", keys[i])).show();
+  if (!$("ul.qwerty li").is(":visible")) {
+    // show alphabet keyboard
+    for (var i = 0; i < keys.length; i++) {
+      if (i < 14) {
+        $("div.keyboard-left ul.qwerty").append(content.replace("$key", keys[i])).show();
 
-            } else {
-                $("div.keyboard-right ul.qwerty").append(content.replace("$key", keys[i])).show();
-            }
-        }
-        $("div.keyboard-right ul.qwerty").append(special_keys_content);
+      } else {
+        $("div.keyboard-right ul.qwerty").append(content.replace("$key", keys[i])).show();
+      }
     }
-    $("ul.qwerty li a").unbind("click");
-    $("ul.qwerty li a").click(function () {
-        copyKeyToSelectedCell(this);
-    });
-    addSpecialKeysClickBehavior();
+    $("div.keyboard-right ul.qwerty").append(special_keys_content);
+  }
+  $("ul.qwerty li a").unbind("click");
+  $("ul.qwerty li a").click(function () {
+    copyKeyToSelectedCell(this);
+  });
+  addSpecialKeysClickBehavior();
 
 }
 
 function addScrollButtonsBehavior() {
-    $('#scrollDown').click(function () {
-        var y = $(window).scrollTop();
-        $(window).scrollTop(y + 50);
-    });
-    $('#scrollUp').click(function () {
-        var y = $(window).scrollTop();
-        $(window).scrollTop(y - 50);
-    });
+  $('#scrollDown').click(function () {
+    var y = $(window).scrollTop();
+    $(window).scrollTop(y + 50);
+  });
+  $('#scrollUp').click(function () {
+    var y = $(window).scrollTop();
+    $(window).scrollTop(y - 50);
+  });
 }
 
 function addSelectionButtonBehavior() {
-    $("#select-btn").click(function () {
-        if (multiSelect) {
-            $("#select-btn span").css('color', 'rgb(153, 1, 0)');
-            multiSelect = false;
-            for (var i = 0; i < selectedCells.length; i++) {
-                var cell = selectedCells[i];
-                cell.removeClass("cell-selected");
-            }
-            selectedCells = [];
-        } else {
-            $("#select-btn span").css('color', 'black');
-            multiSelect = true;
+  $("#select-btn").click(function () {
+    if (multiSelect) {
+      $("#select-btn span").css('color', 'rgb(153, 1, 0)');
+      multiSelect = false;
+      for (var i = 0; i < selectedCells.length; i++) {
+        selectedCells[i].removeClass("cell-selected");
+      }
+      selectedCells = [];
+    } else {
+      $("#select-btn span").css('color', 'black');
+      multiSelect = true;
 
-        }
-    });
+    }
+  });
 }
--- js/utils.js
+++ js/utils.js
@@ -17,80 +17,80 @@ var rowsNumber = 0;
 
 function drawGrid(w, h) {
 
-    var grid = $('#grid');
-    var grid_width, grid_height;
+  var grid = $('#grid');
+  var grid_width, grid_height;
 
-    grid.empty();
+  grid.empty();
 
-    if (w === 0) {
-        grid_width = grid.width();
-    } else {
-        grid_width = w;
+  if (w === 0) {
+    grid_width = grid.width();
+  } else {
+    grid_width = w;
+  }
+  if (h === 0) {
+    grid_height = grid.height();
+  } else {
+    grid_height = h;
+  }
+
+  var cell = "<td class='cell'></td>";
+
+
+  rowsNumber = Math.floor(grid_height / CELL_SIDE);
+  columnsNumber = Math.floor(grid_width / CELL_SIDE);
+  for (let i = 0; i < rowsNumber; i++) {
+    for (let j = 0; j < columnsNumber; j++) {
+      grid.append(cell);
     }
-    if (h === 0) {
-        grid_height = grid.height();
-    } else {
-        grid_height = h;
-    }
-
-    var cell = "<td class='cell'></td>";
-
-
-    rowsNumber = Math.floor(grid_height / CELL_SIDE);
-    columnsNumber = Math.floor(grid_width / CELL_SIDE);
-    for (let i = 0; i < rowsNumber; i++) {
-        for (let j = 0; j < columnsNumber; j++) {
-            grid.append(cell);
-        }
-    }
-    addCellClickBehavior(); // flag current cell selected
+  }
+  addCellClickBehavior(); // flag current cell selected
 
 }
 
 function flagCell(cell) {
 
-    if (multiSelect) {
-        if (selectedCells.length < 1 && selectedCell) {
-            selectedCells.push(selectedCell);
-        }
-        if (cell.hasClass("cell-selected")) {
-            cell.removeClass("cell-selected");
-            selectedCells.splice(selectedCells.indexOf(cell), 1);
-        } else {
-            cell.addClass("cell-selected");
-            selectedCells.push(cell);
-        }
-    } else {
-        cell.addClass("cell-selected");
-        if (selectedCell && cell !== selectedCell) {
-            selectedCell.removeClass("cell-selected");
-        }
-        selectedCell = cell;
+  if (multiSelect) {
+    if (selectedCells.length < 1 && selectedCell) {
+      selectedCells.push(selectedCell);
     }
+    if (cell.hasClass("cell-selected")) {
+      cell.removeClass("cell-selected");
+      selectedCells.splice(selectedCells.indexOf(cell), 1);
+    } else {
+      cell.addClass("cell-selected");
+      selectedCells.push(cell);
+    }
+  } else {
+    cell.addClass("cell-selected");
+    if (selectedCell && cell !== selectedCell) {
+      selectedCell.removeClass("cell-selected");
+    }
+    selectedCell = cell;
+  }
 }
 
 function getPreviousFilledCell() {
-    if (selectedCell) {
-        return selectedCell.prevAll("td.filled-cell:eq(0)");
-    }
+  if (selectedCell) {
+    return selectedCell.prevAll("td.filled-cell:eq(0)");
+  }
 }
 
 function getCurrentCell() {
-    if (selectedCell) {
-        return selectedCell;
-    }
+  if (selectedCell) {
+    return selectedCell;
+  }
 }
 
 function getNextCell() {
-    return selectedCell.next("td.cell");
+  return selectedCell.next("td.cell");
 }
 
 function isIndexingMode(previousFilledCell) {
-    if (previousFilledCell) {
-        return previousFilledCell.data("indexingUpRightApplied") ||
-            previousFilledCell.data("indexingUpLeftApplied") ||
-            previousFilledCell.data("indexingDownApplied") ||
-            (previousFilledCell.data("rootIndexingApplied") && rootMode);
-    }
-    return false;
+  if (previousFilledCell) {
+    return previousFilledCell.data("indexingUpRightApplied") ||
+      previousFilledCell.data("indexingUpLeftApplied") ||
+      previousFilledCell.data("indexingDownApplied") ||
+      (previousFilledCell.data("rootIndexingApplied") && rootMode);
+  }
+  return false;
 }
